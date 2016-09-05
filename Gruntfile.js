ae6d1f4c38d4a90e6c0250a3874752ba5f1d9b92
Christophe Vimbert <c.vimbert@outlook.fr>
2016-09-05 15:16:17 +0200

    Html mockups

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,48 @@
 /**
  * Created by Christophe on 04/09/2016.
  */
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        cordovacli: {
+            options: {
+                path: 'cordova',
+                cli: 'cordova'
+            },
+            cordova: {
+                options: {
+                    command: ['create','platform'],
+                    platforms: ['android'],
+                    path: 'cordova',
+                    id: 'com.blip.gameeditor',
+                    name: 'Blip'
+                }
+            },
+            create: {
+                options: {
+                    command: 'create',
+                    id: 'com.blip.gameeditor',
+                    name: 'Blip'
+                }
+            },
+            build_android: {
+                options: {
+                    command: 'build',
+                    platforms: ['android']
+                }
+            },
+            emulate_android: {
+                options: {
+                    command: 'emulate',
+                    platforms: ['android'],
+                    args: ['--target','Nexus5']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-cordovacli');
+
+    grunt.registerTask("dist", ['requirejs:dist']);
+
+};
