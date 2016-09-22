a91b3d92ce204cadf1a416ce3e20e38603a3d034
Christophe Vimbert <c.vimbert@outlook.fr>
2016-09-22 18:44:20 +0200

    Adding GraphNode

---
+++ src/game/graphs/GraphNode.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Christophe on 22/09/2016.
+ */
+define(["underscore", "eventdispatcher"], function(_, EventDispatcher) {
+
+    return {
+
+        console: {
+            prefix: "graphnode",
+            commands: {
+
+            }
+        },
+        "GraphNode": function(model, modelManager, gameManager) {
+            _.extend(this, EventDispatcher);
+
+            this.initialize = function() {
+
+            };
+        }
+    }
+});
